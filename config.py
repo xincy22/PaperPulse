dbde3827771358dc1e624ae6019188c0ca77a259
xincy22 <xincy22@mails.tsinghua.edu.cn>
2024-12-12 16:46:59 +0800

    Refactor configuration and remove unused files
    - Updated `config.py` to remove hardcoded API keys and replace them with dynamic directory paths for data storage.
    - Deleted `README.md`, `scratch.py`, and various `.idea` configuration files as part of project cleanup.
    - Removed API-related classes and database management code from `api_manager`, `scraper`, `llm`, and `translate` directories, streamlining the codebase.
    - This commit focuses on improving project structure and removing unnecessary components.

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
-import api_manager.database as db
+import os
 
-storage_path = None
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
-db.insert_api_key(api_name="Baidu Translate", app_id="20240227001975820", secret_key="wRu2MkC9BM44Hyo35Jmo",
-                  base_url="https://fanyi-api.baidu.com/api/trans/vip/translate")
-db.insert_api_key(api_name="ChatGLM", api_key="4d701b71cb8f865ee09048d83a16de25.Yg7yClqiDqNOVMTy",
-                  base_url="https://chatglm.example.com/api")
-db.insert_api_key(api_name="CORE API", api_key="jTdkuvoH7BKncXDaJ6byCgzV4tUE2rlI",
-                  base_url="https://api.core.ac.uk/v3/search/works")
-db.insert_api_key(api_name="arXiv API", base_url="https://export.arxiv.org/api/query")
-db.insert_api_key(api_name="CrossRef API", base_url="https://api.crossref.org/works")
+data_dir = os.path.join(base_dir, 'data')
+raw_data_dir = os.path.join(data_dir, 'raw')
+processed_data_dir = os.path.join(data_dir, 'processed')
+cache_data_dir = os.path.join(data_dir, 'cache')
